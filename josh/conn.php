8279fac87bf923ebd985604ae9ef6c88a7d5c044
Josh M <josh@cardiffbank.com>
2015-05-07 08:42:14 -0700

    Test

---
+++ josh/conn.php
@@ -0,0 +1,2 @@
+<?php
+$conn = mysqli_connect('dev.crdff.net', 'josh', 'espada98');
--- josh/xml_out.php
+++ josh/xml_out.php
@@ -1,16 +1,23 @@
 <?php
-$dst = $_REQUEST['To'];
-$src = $_REQUEST['CLID'];
-$conn = mysqli_connect('dev.crdff.net', 'josh', 'espada98');
+require_once 'plivo.php';
+require 'conn.php';
 
 // XML
 Header('Content-type: text/xml');
-$response = new SimpleXMLElement('<Response/>');
-$hangup = $response->addChild('Hangup');
-    $hangup->addAttribute('schedule', '25');
-    $hangup->addAttribute('reason', 'rejected');
-$play = $response->addChild('Play','http://morenojoshua.com/webphone/mmpr.mp3');
-print($response->asXML());
+
+$r = new Response();
+$url = $_SERVER['DOCUMENT_ROOT'] . '/josh/mmpr.mp3';
+$attr = array(
+    'loop' => 2,
+);
+
+$r->addPlay($url, $attr);
+$wait = array(
+    'lenght' => 2,
+);
+$r->addWait($wait);
+header('Content-type: text/xml');
+echo($r->toXML());
 
 //SQL
 // Saves all variables on a sql db, if a record with UUID exists, it is updated
